2819d981567a0a18654468165e780f11f883b549
Joseph Eggers <eggers.joseph23@gmail.com>
2023-05-10 00:14:15 -0700

    updates to readme and build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@
 
 echo "Link object files using the gcc Linker standard 2017"
 
-clang++ -std=c++17 main.cc playerCharacter_/player.cc opponentCharacter_/opponent.cc game_element.cc game.cc cpputils/graphics/image.cc -o main  -lm -lX11 -lpthread
+clang++ -std=c++17 main.cc playerCharacter_/player.cc opponentCharacter_/opponent.cc game_element.cc game.cc cpputils/graphics/image.cc -o "Gnome Run"  -lm -lX11 -lpthread
 
 echo "Built gnomeGame"
